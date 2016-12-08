cafe05671e8122874f7ce6b1541dac639b8ad911
Ilya Smelkov <ismelkov@restream.rt.ru>
2016-12-08 13:25:49 +0300

    Relative paths 🏁

--- config.rb
+++ config.rb
@@ -19,6 +19,9 @@ page '/instr.html', layout: :layout
 
 page '/tutorials/*', content_type: 'text/html'
 
+activate :relative_assets
+set :relative_links, true
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
